95f95896dc683881f7f29598cbdcf6241b7627e6
Cole Zandbergen <zandbergen.cole@gmail.com>
2022-02-17 11:36:45 -0600

    added some comments

--- DOCO-simulation/Main.cpp
+++ DOCO-simulation/Main.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 int main()
 {
+	//first problem is that we need to instantiate the parser and read something from it
+	//just to make sure it works
+	//Can't compile this on mac but i added these comments on my mac!
+	//pushing these to github to make sure they work on vs later...
 	DataParser* parser = parser->getInstance("DOCOData01.xml");
 	cout << parser->getFoodCount() << endl;
 	return 0;

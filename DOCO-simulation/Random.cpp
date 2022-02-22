c30c4befa023a3aaf28634577dd636df67975a2c
Cole <zandebergen.cole@gmail.com>
2022-02-22 12:43:34 -0600

    random should now work (remember srand(time(0)))

--- DOCO-simulation/Random.cpp
+++ DOCO-simulation/Random.cpp
@@ -5,11 +5,13 @@
 
 #include <random>
 
+
 using namespace std;
 
 static int random(int min, int max) {
-	random_device dev;
-	mt19937 rng(dev);
-	uniform_int_distribution<mt19937::result_type> dist(min, max);
-	return dist(rng);
+    int size = max - min + 1;
+    int n = rand() % size;
+    n += min;
+	cout << "Generated the random number " << n << endl;
+	return n;
 }
